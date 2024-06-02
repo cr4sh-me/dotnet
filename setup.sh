f813e19dfd0e3d52629bdcdce443f31394fc71f2
cr4sh <111238843+cr4sh-me@users.noreply.github.com>
2024-06-02 13:24:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ TARGET_ARCH="$(uname -m)"
 
 if [ "$TARGET_ARCH" == "aarch64" ]; then
     wget https://download.visualstudio.microsoft.com/download/pr/54e5bb2e-bdd6-496d-8aba-4ed14658ee91/34fd7327eadad7611bded51dcda44c35/dotnet-sdk-8.0.300-linux-arm64.tar.gz
-elif [ "$TARGET_ARCH" == "armv6l" ] || [ "$TARGET_ARCH" == "armv7l" ]; then
+elif [ "$TARGET_ARCH" == "armv7l" ]; then
     wget https://download.visualstudio.microsoft.com/download/pr/d915a052-b50b-4305-978c-fbf644e71eed/de3ac6fbebd4db705413aac807aa11fe/dotnet-sdk-8.0.300-linux-arm.tar.gz
 else
     echo "$TARGET_ARCH not supported yet!"
